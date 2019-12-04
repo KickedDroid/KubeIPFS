7106d469973147510673f412bbdcab23adce3a2f
Aidan <asmaidan@look.com>
2019-12-04 16:18:12 -0600

    Update run.sh

--- temporal/testenv/run.sh
+++ temporal/testenv/run.sh
@@ -5,7 +5,27 @@ set -e
 echo "Deploying Temporal...."
 kubectl apply -f test-env.yaml
 
-sleep 10
+
+echo "Waiting for all containers to be running..."
+
+while true; do
+    sleep 10
+    statuses=`kubectl get pods -l 'app=temporal' -o jsonpath='{.items[*].status.phase}' | xargs -n1`
+    echo $statuses
+    all_running="yes"
+    for s in $statuses; do
+        if [[ "$s" != "Running" ]]; then
+            all_running="no"
+        fi
+    done
+    if [[ $all_running == "yes" ]]; then
+        break
+    fi
+done
+
+
+
+sleep 5
 
 kubectl get all 
 
