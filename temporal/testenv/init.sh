ef35b425f4364a84a707dfe0217b48ce42471646
Aidan <asmaidan@look.com>
2019-12-05 17:54:27 -0600

    Update init.sh

--- temporal/testenv/init.sh
+++ temporal/testenv/init.sh
@@ -15,6 +15,8 @@ echo "
 
 "
 
+echo "Inititalizing prerequisites....."
+
 echo "Generating erlang cookie..."
 kubectl create secret generic rabbitmq-config --from-literal=erlang-cookie=c-is-for-cookie-thats-good-enough-for-me
 
