ebda3b5e319cbe64872b8479a6589e0c979be89b
Aidan <asmaidan@look.com>
2020-03-10 15:01:37 -0500

    Fixed config issue
    - Mergerd run.sh into init.sh
    - added service to test-env.yaml
    - changed DATABASE_URL to default namespace

--- temporal/testenv/init.sh
+++ temporal/testenv/init.sh
@@ -31,7 +31,32 @@ cat postgres.yaml | linkerd inject - | kubectl apply -f -
 
 kubectl apply -f temporal-config.yaml
 
-kubectl get services
+kubectl get services 
+
+sleep 10 
+echo "Deploying Temporal...."
+cat test-env.yaml | linkerd inject - | kubectl apply -f -
+
+
+echo "Waiting for all containers to be running..."
+
+while true; do
+    sleep 10
+    statuses=`kubectl get pods -l 'app=temporal' -o jsonpath='{.items[*].status.phase}' | xargs -n1`
+    echo $statuses
+    all_running="yes"
+    for s in $statuses; do
+        if [[ "$s" != "Running" ]]; then
+            all_running="no"
+        fi
+    done
+    if [[ $all_running == "yes" ]]; then
+        break
+    fi
+done
+
+kubectl get services 
+kubectl get pods 
 
 
 set +ex
